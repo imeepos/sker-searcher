a1174fa501d0fa2c3cb0d7bda0363955788b5091
ymm <ymm@bowang.com>
2025-03-28 12:21:50 +0800

    fix

--- packages/tools/src/index.ts
+++ packages/tools/src/index.ts
@@ -1,12 +1,30 @@
 
 import { SiliconflowChatCompletionsTool } from '@sker/core'
 import { useQuery as _useQuery } from '@sker/orm'
-
+import { useSandbox } from '@sker/sandbox'
 const useQuery = async (sql: string, parameters: any[]) => {
     return _useQuery([], sql, parameters)
 }
 export async function useTools(): Promise<SiliconflowChatCompletionsTool[]> {
     return [
+        {
+            type: 'function',
+            function: {
+                strict: false,
+                name: 'useSandbox',
+                description: `Execute a javascript code with parameters`,
+                parameters: {
+                    type: "object",
+                    properties: {
+                        code: {
+                            type: "string",
+                            description: "The javascript code to execute"
+                        }
+                    },
+                    required: ["code"]
+                }
+            }
+        },
         {
             type: 'function',
             function: {
@@ -35,11 +53,14 @@ export async function useTools(): Promise<SiliconflowChatCompletionsTool[]> {
     ]
 }
 
-export async function runTool(name: string, parameters: any[]) {
+export async function runTool(name: string, ...parameters: any[]) {
     switch (name) {
         case "useQuery":
             const { sql, params } = parameters[0]
             return await useQuery(sql, params)
+        case "useSandbox":
+            const { code } = parameters[0]
+            return useSandbox(code)
         default:
             console.log({ name, parameters })
     }

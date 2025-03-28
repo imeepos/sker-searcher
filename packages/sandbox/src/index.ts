e5744a6176ccc5f07d821ea286e5443c5abeec71
ymm <ymm@bowang.com>
2025-03-28 16:08:25 +0800

    fix

--- packages/sandbox/src/index.ts
+++ packages/sandbox/src/index.ts
@@ -2,6 +2,9 @@ import { createContext, Script } from 'vm'
 
 export function useSandbox(code: string, context: any = {}) {
     const script = new Script(code)
+    /**
+     * 查询数据库中的ai_package_name和ai_code
+     */
     const ctx = createContext({
         ...context,
         require: require,
